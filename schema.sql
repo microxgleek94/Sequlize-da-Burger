d89d3041ab2994f829f8346bca92eb40719b9f7e
Keiana Hanson <kiki_hanson@aol.com>
2020-08-11 13:56:51 -0400

    testing heroku deployment

--- schema.sql
+++ schema.sql
@@ -4,3 +4,19 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 -- Use db if created already --
 USE burgers_db;
+
+-- Default Table --
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (`burger_name`) VALUE ('Mega-Man 3 Meats Burger');
+INSERT INTO burgers (`burger_name`) VALUE ('Metroid "Out of this World" Beyond Burger');
+INSERT INTO burgers (`burger_name`) VALUE ('Portobello Burger');
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ("Spongebob's Double Patty Angus Beef Burger", true);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ('Mission Impossible Mango Burger', true);
+INSERT INTO burgers (`burger_name`, `devoured`) VALUE ("Bowser's BBQ Chicken Burger", true);
+
