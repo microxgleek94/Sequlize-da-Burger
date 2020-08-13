583b5d48269d984e06a318b58cd23a81122f926b
Keiana Hanson <kiki_hanson@aol.com>
2020-08-13 00:33:25 -0400

    submit btn and data displaying correctly, working on post/update function

--- app/public/js/burger_frontend_controller.js
+++ app/public/js/burger_frontend_controller.js
@@ -1,94 +1,68 @@
+// will show default table data, and any new data entered moving forward
 $.get("/api/all", function (data) {
-  console.log(`defalut table values: ${JSON.stringify(data)}`); 
+  console.log(`defalut table values: ${JSON.stringify(data)}`);
 
   for (var i = 0; i < data.length; i++) {
     // Create a parent div to hold default burger data &Add an id to the div to mark which div it is
-    var defaultBurgDiv = $("<div>").addClass("well").attr("id", "burger-well-" + i);
-  
-    var devourBtn = $("<button>").attr({ type: "button", class: "btn btn-info" }).text("Devour Me!");
+    var defaultBurgDiv = $("<div>").addClass("well").attr("id", "burger-well- " + i);
+    var devourBtn = $("<button>").attr({ type: "button", class: "btn btn-info", id: "devour-btn" + i }).text("Devour Me!");
     var defaultBurgName = $("<h5>" + (i + 1) + ". " + data[i].burger_name + "</h5>").attr("id", "defaultText");
     $(defaultBurgDiv).append(defaultBurgName, devourBtn);
     $("#devour-body").append(defaultBurgDiv);
   }
+
+  
 });
 
 
-    // // user creates a new burger
-    // $("#submit-btn").on("click", function (event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
+//user creates a new burger 
+// and this function runs after the "Submit" btn is clicked
+$("#submit-btn").on("click", function (event) {
+  // Make sure to preventDefault on a submit event.
+  console.log("submit btn clicked!")
+  event.preventDefault();
 
-    //   var newBurger = {
-    //     burger_name: $("#textbox").val().trim(),
-    //     devoured: 0
-    //   };
+  var newBurger = {
+    burger_name: $("#textbox").val().trim(),
+    // setting the default value to 0 so the burger is not "devoured" as yet
+    devoured: 0
+  };
 
-    //   // Send the POST request.
-    //   $.ajax("/api/newburger", {
-    //     type: "POST",
-    //     data: newBurger
-    //   }).then(
-    //     function () {
-    //       console.log(`new burger created: ${newBurger.burger_name}`);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+  // Send the POST request.
+  $.post("/api/newburger", newBurger)
+    .then(
+      function (data) {
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+});
 
-    // $(".clone-burger").on("click", function(event) {
-    //   var burgerName = $(this).data("burger_name");
+// User presses the devour btn to move burger to devour bin
+$(".btn-info").on("click", function (event) {
+  console.log("devour btn was clicked!");
+  var id = $(this).data("id");
 
-    //   var clonedBurger = {
-    //     burger_name: burgerName,
-    //     devoured: 0
-    //   };
+  var burgerChanged = {
+    devoured: 1
+  };
 
-    //   // Send the POST request.
-    //   $.ajax("/api/newburger", {
-    //     type: "POST",
-    //     data: clonedBurger
-    //   }).then(
-    //     function() {
-    //       console.log("Cloned New Burger");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+  // Send the PUT request.
+  $.put("/api/burger/" + id, burgerChanged).then(
+    function (data) {
+      console.log("changed to devoured ", burgerChanged);
+      if (data.devoured == 1) {
+        var devourBurgDiv = $("<div>").addClass("wellDevour").attr("id", "burger-devour- " + i);
+        var deleteBtn = $("<button>").attr({ type: "button", class: "btn btn-dark", id: "delete-btn" + i }).text("Delete Me!");
+        var devourBurgName = $("<h5>" + (i + 1) + ". " + burgerChanged.burger_name + "</h5>").attr("id", "changedBurg");
+        $(devourBurgDiv).append(devourBurgName, deleteBtn);
+        $("#devour-bin").append(devourBurgDiv);
+      }
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
 
-    // $(".delete-burger").on("click", function(event) {
-    //   var id = $(this).data("id");
 
-    //   // Send the PUT request.
-    //   $.ajax("/api/burger/" + id, {
-    //     type: "DELETE",
-    //     data: {id:id}
-    //   }).then(
-    //     function() {
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
-
-    // $(".devour-burger").on("click", function(event) {
-    //   var id = $(this).data("id");
-
-    //   var burgerChanged = {
-    //     devoured: 1
-    //   };
-
-    //   // Send the PUT request.
-    //   $.ajax("/api/burger/devour/" + id, {
-    //     type: "PUT",
-    //     data: burgerChanged
-    //   }).then(
-    //     function() {
-    //       console.log("changed to devoured ", burgerChanged);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+});
 
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -19,41 +19,32 @@ app.get("/api/all", function(req, res) {
   });
 });
 
-
-  // app.post("/api/newburger", function(req, res) {
-  //   // console.log(req.body);
-  //   db.Burger.create({
-  //       // squelize is creating a new burger in the table 
-  //       // w/ the attributes we defined in the Burger model
-  //     burger_name: req.body.burger_name,
-  //     devoured: req.body.devoured
-  //   }).then (function(result) {
-  //     // Send back the ID of the new quote
-  //     res.json({ id: result.insertId });
-  //   });
-  // });
+// saves user data for new burger creation
+app.post("/api/newburger", function(req, res) {
+    
+  console.log(`New Burger Data: ${JSON.stringify(req.body)}`);
+    db.create({
+        // squelize is creating a new burger in the table 
+        // w/ the attributes we defined in the Burger model
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }).then(function(result) {
+      // Send back the ID of the new quote
+      res.json({id: result.insertId });
+    });
+  });
   
-  // app.delete("/api/burger/:id", function(req, res) {
-  //   db.Burger.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function (result) {
-  //     res.json(result);
-  //   });
-  // });
-  
-  // app.put("/api/burger/devour/:id", function(req, res) {
-  //   db.Burger.update(
-  //     {
-  //       devoured: req.body.devoured
-  //     },
-  //     {
-  //       where: {
-  //         id: req.params.id
-  //       }
-  //     }).then(function (result) {
-  //       res.json(result);
-  //     });
-  //   });
+  app.put("/api/burger/:id", function(req, res) {
+    db.update(
+      {
+        devoured: req.body.devoured
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then(function (result) {
+        res.json(result);
+      });
+    });
 };
